f892056d24fa363b98a57219c18705ca81e30c7c
Jane Choi <janechoi@192-168-1-105.tpgi.com.au>
2020-03-04 23:34:33 +1000

    added activity_button

---
+++ app/src/main/java/com/example/kioskjb/aroundStore.java
@@ -0,0 +1,13 @@
+package com.example.kioskjb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aroundStore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_around_store);
+    }
+}
